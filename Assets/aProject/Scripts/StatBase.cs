c5f85415a0d7bcbb64c9e0507ec3dbab7f4dca95
Andrzej Kebab <grzesiugora123@wp.pl>
2023-12-20 23:11:27 +0100

    github ptsd

---
+++ Assets/aProject/Scripts/StatBase.cs
@@ -0,0 +1,22 @@
+﻿using Sirenix.OdinInspector;
+using UnityEngine;
+
+namespace PatataStudio
+{
+	[System.Serializable]
+	public struct StatBase
+	{
+		[ProgressBar(0, "Max", Height = 15)]
+		public float Current;
+		[HideInInspector] public float Max;
+		public float Base;
+		public float Multiplier;
+		public float Additive;
+
+		public void RecalculateStat()
+		{
+			Max = (Base * Multiplier) + Additive;
+			Current = Max;
+		}
+	}
+}
